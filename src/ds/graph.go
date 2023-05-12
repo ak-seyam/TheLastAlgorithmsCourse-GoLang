40405fc06136e73fdce98347c067eace01e9886b
Abdullah Khaled <abdullah.khaled.kamal0@gmail.com>
2023-05-12 19:02:50 +0300

    feat: add DFS to Adj Graph

--- src/ds/graph.go
+++ src/ds/graph.go
@@ -1,13 +1,13 @@
 package ds
 
-type AdjListConnection struct {
-	To     int
-	Weight int
+type AdjListNode[T interface{}] struct {
+	Connections []AdjListConnection[T]
+	Value       T
 }
 
-type AdjListNode[T interface{}] struct {
-	Connections []AdjListConnection
-	Value       T
+type AdjListConnection[T interface{}] struct {
+	To     *AdjListNode[T]
+	Weight int
 }
 
 type AdjMatrixConnection struct {
@@ -17,4 +17,30 @@ type AdjMatrixConnection struct {
 type AdjMatrixNode[T interface{}] struct {
 	Connection AdjMatrixConnection
 	Value      T
+}
+
+type AdjListTraversalCallBack[T interface{}] func(n *AdjListNode[T])
+
+func (a AdjListNode[T]) DFS(cb AdjListTraversalCallBack[T]) {
+	s := NewStack[*AdjListNode[T]]()
+	s.Push(&a)
+	for !s.IsEmpty() {
+		node := s.Pop()
+		cb(node)
+		for _, con := range node.Connections {
+			s.Push(con.To)
+		}
+	}
+}
+
+func (a AdjListNode[T]) BFS(cb AdjListTraversalCallBack[T]) {
+	s := NewQueue[*AdjListNode[T]]()
+	s.Enqueue(&a)
+	for !s.IsEmpty() {
+		node := s.Dequeu()
+		cb(node)
+		for _, con := range node.Connections {
+			s.Enqueue(con.To)
+		}
+	}
 }
---
+++ src/ds/stack.go
@@ -0,0 +1,28 @@
+package ds
+
+type Stack[T interface{}] struct {
+	topIdx int
+	data   []T
+}
+
+func (s *Stack[T]) Push(item T) {
+	s.data = append(s.data, item)
+	s.topIdx++
+}
+
+func (s *Stack[T]) Pop() T {
+	res := s.data[s.topIdx]
+	s.topIdx--
+	return res
+}
+
+func NewStack[T interface{}]() Stack[T] {
+	return Stack[T]{
+		topIdx: -1,
+		data:   []T{},
+	}
+}
+
+func (s Stack[T]) IsEmpty() bool {
+	return s.topIdx == -1
+}
